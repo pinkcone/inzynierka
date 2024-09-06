d0d5f19c25b030e8eb5d907db4693c87431df9c0
pinkcone <szymekdudek4@gmail.com>
2024-09-06 16:37:21 +0200

    added node and react

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the server!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
