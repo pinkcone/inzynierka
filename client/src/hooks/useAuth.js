c9e261ab6ea2eb28d7255f9b506fdc4e171819ad
pinkcone <szymekdudek4@gmail.com>
2024-12-21 22:10:46 +0100

    fixes

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {jwtDecode} from 'jwt-decode';
 import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const useAuth = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
-  const [isAdmin, setIsAdmin] = useState(false); 
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [sessionExpired, setSessionExpired] = useState(false); // flaga
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,9 +17,10 @@ const useAuth = () => {
         const decodedToken = jwtDecode(token);
         const currentTime = Date.now() / 1000;
 
-        if (decodedToken.exp < currentTime) {
-          setIsAuthenticated(false); // Ustawia stan lokalny bez wywoływania `logout`
+        if (decodedToken.exp < currentTime && !sessionExpired) {
+          setSessionExpired(true);
           toast.warn('Twoja sesja wygasła. Zaloguj się ponownie.');
+          setIsAuthenticated(false);
           navigate('/login');
         } else {
           setUser(decodedToken);
@@ -28,9 +29,10 @@ const useAuth = () => {
 
           const interval = setInterval(() => {
             const timeLeft = decodedToken.exp - Date.now() / 1000;
-            if (timeLeft <= 0) {
-              setIsAuthenticated(false); // Zatrzymuje dalsze interakcje
+            if (timeLeft <= 0 && !sessionExpired) {
+              setSessionExpired(true);
               toast.warn('Twoja sesja wygasła. Zaloguj się ponownie.');
+              setIsAuthenticated(false);
               navigate('/login');
             }
           }, 480000);
@@ -44,30 +46,23 @@ const useAuth = () => {
         setIsAdmin(false);
       }
     }
-  }, [navigate]);
-
-
+  }, [navigate, sessionExpired]);
 
   const logout = (message) => {
     if (!localStorage.getItem('token')) {
       console.warn('Użytkownik już wylogowany.');
       return;
     }
-
     localStorage.removeItem('token');
     localStorage.setItem('logoutMessage', message);
     setIsAuthenticated(false);
     setUser(null);
     setIsAdmin(false);
-
     navigate('/');
-
     setTimeout(() => {
       window.location.reload();
-    }, 100); // Krótkie opóźnienie dla bezpieczeństwa
+    }, 100);
   };
-
-
 
   return { isAuthenticated, user, isAdmin, logout };
 };
